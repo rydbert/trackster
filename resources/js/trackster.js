055f76274b8d8330358b6683c464ceade732e540
Erik <erikrydbert@outlook.com>
2018-01-11 17:15:22 +0800

    Finished-ish

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,4 +1,5 @@
 var Trackster = {};
+const API_KEY = "0f726d3865386d31cc3c03fa7f1fdd57";
 
 /*
 In trackster.js, use jQuery's .click() method to handle the user
@@ -25,7 +26,28 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#track-list');
 
+  $trackList.empty();
+
+  for (trackIndex=0; trackIndex<tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]["#text"];
+    var htmlTrackRow =
+    '<div class="row track">' +
+    ' <div class="col-xs-1 col-xs-offset-1 play-button">' +
+    '   <a href="' + track.url + '" target="_blank">' +
+    '     <i class="fa fa-play-circle-o fa-2x"></i>' +
+    '   </a>' +
+    ' </div>' +
+    ' <div class="col-xs-4 attribute">' + track.name + '</div>' +
+    ' <div class="col-xs-2 attribute">' + track.artist + '</div>' +
+    ' <div class="col-xs-2 attribute"><img src=' + mediumAlbumArt + ' alt="Album art for ' + track.name + ' by ' + track.artist + '"></div>' +
+    ' <div class="col-xs-2 attribute">' + track.listeners + '</div>' +
+    '</div';
+
+    $trackList.append(htmlTrackRow);
+  }
 };
 
 /*
@@ -33,10 +55,10 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-  console.log(title);
-
-/*  $.ajax("http://ws.audioscrobbler.com/2.0/?method=track.search&track=").concat(title).concat("&api_key=").concat(API_KEY).concat("&format=json"));
-*/
-  $.ajax("http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json"));
-
+  $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
+  });
 };
